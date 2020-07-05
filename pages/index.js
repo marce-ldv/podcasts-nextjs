760e6df69b754b8a48d6ded333eddf630acb317b
MarceloLudovino <ext-m.ludovino@rappi.com>
2020-07-05 16:40:48 -0300

    Add channels

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,19 @@
 import Head from 'next/head'
+import 'isomorphic-fetch'
 
-export default function Home() {
+export async function getServerSideProps() {
+  let req = await fetch('https://api.audioboom.com/channels/recommended');
+  let { body: channels } = await req.json();
+
+  return { props: { channels: channels } };
+}
+
+const Home = ({ channels }) => {
+  console.log(channels)
   return (
     <div className="container">
       <Head>
-        <title>Create Next App</title>
+        <title>Podcasts Nextjs</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
@@ -12,147 +21,60 @@ export default function Home() {
         <h1 className="title">
           Podcasts
         </h1>
+        {channels.map(({urls, title}) => (
+          <div className="channel">
+            <img src={urls.logo_image.original} alt={title} />
+            <h2>{title}</h2>
+          </div>
+        ))}
       </main>
 
       <footer>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className="logo" />
+        <a target="_blank" rel="noopener noreferrer">
+          Powered by Marcelo
         </a>
       </footer>
 
       <style jsx>{`
-        .container {
-          min-height: 100vh;
-          padding: 0 0.5rem;
-          display: flex;
-          flex-direction: column;
-          justify-content: center;
-          align-items: center;
-        }
-
-        main {
-          padding: 5rem 0;
-          flex: 1;
-          display: flex;
-          flex-direction: column;
-          justify-content: center;
-          align-items: center;
-        }
-
-        footer {
-          width: 100%;
-          height: 100px;
-          border-top: 1px solid #eaeaea;
-          display: flex;
-          justify-content: center;
-          align-items: center;
-        }
-
-        footer img {
-          margin-left: 0.5rem;
-        }
-
-        footer a {
-          display: flex;
-          justify-content: center;
-          align-items: center;
-        }
-
-        a {
-          color: inherit;
-          text-decoration: none;
-        }
-
-        .title a {
-          color: #0070f3;
-          text-decoration: none;
-        }
-
-        .title a:hover,
-        .title a:focus,
-        .title a:active {
-          text-decoration: underline;
-        }
-
-        .title {
-          margin: 0;
-          line-height: 1.15;
-          font-size: 4rem;
-        }
-
-        .title,
-        .description {
+      header {
+          color: #fff;
+          background: #8756ca;
+          padding: 15px;
           text-align: center;
         }
 
-        .description {
-          line-height: 1.5;
-          font-size: 1.5rem;
+        .channels {
+          display: grid;
+          grid-gap: 15px;
+          padding: 15px;
+          grid-template-columns: repeat(auto-fill, minmax(160px, 1fr));
         }
 
-        code {
-          background: #fafafa;
-          border-radius: 5px;
-          padding: 0.75rem;
-          font-size: 1.1rem;
-          font-family: Menlo, Monaco, Lucida Console, Liberation Mono,
-            DejaVu Sans Mono, Bitstream Vera Sans Mono, Courier New, monospace;
+        .channel {
+          display: block;
+          border-radius: 3px;
+          box-shadow: 0px 2px 6px rgba(0, 0, 0, 0.15);
+          margin-bottom: 0.5em;
         }
 
-        .grid {
-          display: flex;
-          align-items: center;
-          justify-content: center;
-          flex-wrap: wrap;
-
-          max-width: 800px;
-          margin-top: 3rem;
+        .channel img {
+          width: 100%;
         }
 
-        .card {
-          margin: 1rem;
-          flex-basis: 45%;
-          padding: 1.5rem;
-          text-align: left;
-          color: inherit;
-          text-decoration: none;
-          border: 1px solid #eaeaea;
-          border-radius: 10px;
-          transition: color 0.15s ease, border-color 0.15s ease;
-        }
-
-        .card:hover,
-        .card:focus,
-        .card:active {
-          color: #0070f3;
-          border-color: #0070f3;
-        }
-
-        .card h3 {
-          margin: 0 0 1rem 0;
-          font-size: 1.5rem;
-        }
-
-        .card p {
+        .channel h2 {
+          padding: 5px;
+          font-size: 0.9em;
+          font-weight: 600;
           margin: 0;
-          font-size: 1.25rem;
-          line-height: 1.5;
+          text-align: center;
         }
+      `}</style>
 
-        .logo {
-          height: 1em;
-        }
-
-        @media (max-width: 600px) {
-          .grid {
-            width: 100%;
-            flex-direction: column;
-          }
+      <style jsx global>{`
+        body {
+          margin: 0;
+          background: white;
+          font-family: system-ui;
         }
       `}</style>
 
@@ -173,3 +95,5 @@ export default function Home() {
     </div>
   )
 }
+
+export default Home;
